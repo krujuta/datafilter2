416e3e3886dad1ba8166cd5e9a0de6b27eba7a38
Rujuta Karanjkar <karanjkarujuta@gmail.com>
2022-03-09 00:26:43 +0100

    added database

--- datafilter2/settings.py
+++ datafilter2/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['DJANGO_SECRET']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]',
 'datafilter2-heroku.herokuapp.com']
