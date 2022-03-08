cc8d9922d86df42e26dd755c7b6837e6ff4e7f76
Rujuta Karanjkar <karanjkarujuta@gmail.com>
2022-03-08 23:50:08 +0100

    removed extras

--- datafilter2/settings.py
+++ datafilter2/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ['DJANGO_SECRET']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]',
+'datafilter2-heroku.herokuapp.com']
 
 
 # Application definition
