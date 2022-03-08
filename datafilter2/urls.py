c69f56fe568a101d85203ae926c2eb566863ea98
Rujuta Karanjkar <karanjkarujuta@gmail.com>
2022-03-08 23:09:10 +0100

    removed extras

--- datafilter2/urls.py
+++ datafilter2/urls.py
@@ -24,7 +24,6 @@ router.register(r'products', views.ProductsViewSet)
 # Setup automatic URL routing
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
