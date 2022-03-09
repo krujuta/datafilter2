1b92b5ffdd0d5d59cd9efdbdb23f7e9f2d955952
Rujuta Karanjkar <karanjkarujuta@gmail.com>
2022-03-09 12:04:55 +0100

    added db settings

--- datafilter2/settings.py
+++ datafilter2/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'datafilter2',
+    'datafilter2.api',
     'rest_framework',
 ]
 
